f7a4584cfd88367eaf5266c7b1953b581ccbba8b
Dominic Adams <dominicadams@Dominics-MacBook-Pro.local>
2018-01-01 21:50:36 +0000

    Add python scripts and routes

--- src/server/app.js
+++ src/server/app.js
@@ -7,9 +7,35 @@
  ***************************************************** */
 import express from 'express';
 import { join as joinPath } from 'path';
-import router from './server-router';
+import PythonShell from 'python-shell';
+import serverRouter from './server-router';
+
+
+// we'll create our routes here
+
+// get an instance of router
+const router = express.Router();
+
+// home page route (http://localhost:8080)
+router.get('/plugs/2/on', function(req, res) {
+  PythonShell.run('pythonScripts/plug2On.py', function (err) {
+    if (err) throw err;
+    res.send('I turned the plug on!');
+  });
+});
+
+// about page route (http://localhost:8080/about)
+router.get('/plugs/2/off', function(req, res) {
+  PythonShell.run('pythonScripts/plug2Off.py', function (err) {
+    if (err) throw err;
+    res.send('I turned the plug off!');
+  });
+});
+
+// apply the routes to our application
+app.use('/', router);
 
 export default () => express()
   .use(express.static(joinPath(__dirname, 'public')))
-  .use(router());
+  .use(serverRouter());
 
